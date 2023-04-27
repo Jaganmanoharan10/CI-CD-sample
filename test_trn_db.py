6c87b1a69ba9cf159f46e7d3059c51f28d42c60e
Jaganmanoharan10 <131437637+Jaganmanoharan10@users.noreply.github.com>
2023-04-27 23:49:53 +0530

    Update test_trn_db.py
    Signed-off-by: Jaganmanoharan10 <131437637+Jaganmanoharan10@users.noreply.github.com>

--- test_trn_db.py
+++ test_trn_db.py
@@ -2,13 +2,13 @@
 # Employee hire date should not be greater than current date
 import pytest
 def test_hire_date(trn_cursor):
-    trn_cursor.execute("select hire_date from TRN.hr.employees where hire_date > GETDATE()")
+    trn_cursor.execute("select hire_date from hr.employees where hire_date > GETDATE()")
     hire_date = trn_cursor.fetchall()
     assert len(hire_date) == 0, "Employee hire date is greater than the current date"
 
 
 # Employee minimum and maximum salary validation
 def test_min_max_sal(trn_cursor):
-    trn_cursor.execute("select min(salary), max(salary) from trn.hr.employees")
+    trn_cursor.execute("select min(salary), max(salary) from hr.employees")
     min_max_sal = trn_cursor.fetchall()
     assert list(min_max_sal[0]) == [2500.00, 24000.00], "Employee salary is not in range"
